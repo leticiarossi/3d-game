75cf8b5e44ac6a82484f6e9b3b3f66e1b45b0501
Leticia Rossi <lrossidossantos18@amherst.edu>
2017-04-10 08:30:13 +0200

    Room setup test and some organization stuff

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 2f;
+
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertixal = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertixal);
+
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		rb.MovePosition (transform.position + movement);
+	}
+}
