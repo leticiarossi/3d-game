1e13551c00c42287b17e302c452ff78e733ab452
susher325 <sam.usher325@gmail.com>
2017-04-26 23:13:58 +0200

    Candle Lights Up

---
+++ Assets/Scripts/CandleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleController : MonoBehaviour {
+
+	public GameObject candle;
+	public GameObject litCandle;
+
+	// Use this for initialization
+	void Start () {
+		candle.SetActive (true);
+	}
+
+	void OnTriggerEnter (Collider Other){
+		if (Other.tag == "Fireball") {
+			LightCandle ();
+		}
+	}
+
+	void LightCandle(){
+		Vector3 candlePosition = candle.transform.position;
+		Quaternion candleRotation = candle.transform.rotation;
+		Destroy (candle);
+		Instantiate (litCandle, candlePosition, candleRotation);
+	}
+}
