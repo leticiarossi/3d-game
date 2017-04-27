77c7dd44803be6dffbe053c61b2aaf6cb51701f6
Leticia Rossi <lrossidossantos18@amherst.edu>
2017-04-27 09:52:04 +0200

    Change candle stuff and name of scripts and prefabs

---
+++ Assets/Scripts/Candle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candle : MonoBehaviour {
+
+	public GameObject fire;
+	bool isLit = false;
+
+	void Start () {
+		fire.SetActive (false);
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (!isLit && other.tag == "Fireball") {
+			fire.SetActive (true);
+			isLit = true;
+		} else if (isLit && other.tag == "Waterball") {
+			fire.SetActive (false);
+			isLit = false;
+		}
+	}
+}
