cdcbe9899bce99a9b8c77b0475003b0ca2626c8d
susher325 <sam.usher325@gmail.com>
2017-04-27 12:55:43 +0200

    Start of PauseMenu UI

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/*
+ * This class controls the continue button within the pause panel
+ */
+public class ButtonManager : MonoBehaviour {
+
+	public Button continueButton; 
+	public Button exitButton;
+
+	void Start () {
+		Button contBtn = continueButton.GetComponent<Button>();
+		Button exBtn = exitButton.GetComponent<exitButton> ();
+		contBtn.onClick.AddListener(ContinueTaskOnClick);
+		exBtn.onClick.AddListener (ExitTaskOnClick);
+	}
+
+	void ContinueTaskOnClick(){
+		MenuManager.DisablePause (); //references menu manager class using a singleton pattern
+	}
+
+	void ExitTaskOnClick(){
+		SceneManager.LoadScene (0, LoadSceneMode.Single);
+	}
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+
+	public static MenuManager instance;
+	public GameObject PauseMenu;
+
+	void Awake ()
+	{
+		instance = this;
+		PauseMenu.SetActive (false);
+	}
+
+	public static void EnablePause(){
+		instance.PauseMenu.SetActive (true);
+		Time.timeScale = 0; //pause background when panel is present
+	}
+
+	public static void DisablePause(){
+		instance.PauseMenu.SetActive (false);
+		Time.timeScale = 1; //unpause background when panel goes away
+	}
+}
