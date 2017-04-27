334ad14de3b435954ad838500734e27a7737256d
susher325 <sam.usher325@gmail.com>
2017-04-27 12:01:55 +0200

    Main Screen

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+	
+	public Button StartButton;
+	public Button QuitButton;
+
+	void Start()
+	{
+		Button strtBtn = StartButton.GetComponent<Button>();
+		Button qtBtn = QuitButton.GetComponent<Button> ();
+		strtBtn.onClick.AddListener(startTaskOnClick);
+		qtBtn.onClick.AddListener (quitTaskOnClick);
+
+	}
+
+	void startTaskOnClick()
+	{
+		UnityEngine.Application.LoadLevel (1);
+	}
+
+	void quitTaskOnClick()
+	{
+		Application.Quit ();
+	}
+}
